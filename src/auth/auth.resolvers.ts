8b3813a472c098ed1a038b2648d3e6fbf85bce5b
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-03-28 11:52:42 +0000

    signup method

--- src/auth/auth.resolvers.ts
+++ src/auth/auth.resolvers.ts
@@ -1,15 +1,19 @@
-import { Resolvers } from "../__generated__/resolvers-types"
+import { Resolvers } from "../__generated__/resolvers-types";
+import {userService} from './user/user.service';
+import jwt from 'jsonwebtoken'
 
 export const authResolvers: Resolvers = {
     Mutation: {
         async signup(parent, {input}, context) {
-            return {
-                user: {
-                    id: 1,
-                    ...input
-                },
-                jwt: 'dummyfornow'
-            }
+            const user = await userService.create(input);
+
+            const jwtToken = jwt.sign(
+                {email: input.email, userId: user.id}, 
+                process.env.JWT_KEY!,
+                {expiresIn: '7d'}
+            );
+
+            return {user, jwt: jwtToken};
         }
     },
 
