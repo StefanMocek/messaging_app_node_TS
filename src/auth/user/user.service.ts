0eb631757b82fa2e86a7c89b4f74d611dc2c940a
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-03-29 15:58:04 +0000

    finding user by email changed

--- src/auth/user/user.service.ts
+++ src/auth/user/user.service.ts
@@ -1,21 +1,29 @@
 import { Repository } from "typeorm";
 import bcrypt from 'bcrypt';
 import { User } from "./entity/user.entity";
-import {SignupInput} from '../../__generated__/resolvers-types'
-import {AppDataSource} from '../../app-data.source'
+import { SignupInput } from '../../__generated__/resolvers-types'
+import { AppDataSource } from '../../app-data.source'
 
 export class UserService {
-    constructor(public userRepository: Repository<User>) {}
+    constructor(public userRepository: Repository<User>) { }
 
-    async create (signupInput: SignupInput) {
+    async create(signupInput: SignupInput) {
         const password = await bcrypt.hash(signupInput.password, 10);
-        const user = this.userRepository.create({...signupInput, password});
-        
+        const user = this.userRepository.create({ ...signupInput, password });
+
         return await this.userRepository.save(user);
     };
 
-    async findOneByEmail(email:string) {
-        return await this.userRepository.findOneBy({email})
+    async findOneByEmail(email: string) {
+        //only if entity.column({select: false})
+        const user = await this.userRepository
+            .createQueryBuilder('user')
+            .select('user')
+            .addSelect('user.password')
+            .where('email = :email', { email })
+            .getOne()
+
+        return user
     }
 };
 
