cab38ee315c0dec2271bbdd7accbfe77f2cf354a
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-03-29 16:18:35 +0000

    context added to app

--- src/module.ts
+++ src/module.ts
@@ -1,30 +1,55 @@
-import http from 'http'
-import {readFileSync} from 'fs'
+import http from 'http';
+import { readFileSync } from 'fs';
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { Resolvers } from './__generated__/resolvers-types';
+import jwt from 'jsonwebtoken';
+import { ApolloServer, ExpressContext } from "apollo-server-express";
+import { JwtPayload, Resolvers } from './__generated__/resolvers-types';
 import { authResolvers } from './auth/auth.resolvers';
 
-export class AppModule {
-    constructor (public resolvers: Resolvers){}
+export interface MyContext extends ExpressContext {
+    currentUser: JwtPayload;
+    authorized: boolean
+}
 
-    async startApollo():Promise<{httpServer: http.Server, server: ApolloServer}>{
-        const typeDefs = readFileSync('schema.graphql', {encoding: 'utf-8'});
+export class AppModule {
+    constructor(public resolvers: Resolvers) { }
+
+    async startApollo(): Promise<{ httpServer: http.Server, server: ApolloServer<MyContext> }> {
+        const typeDefs = readFileSync('schema.graphql', { encoding: 'utf-8' });
 
         const app = express();
 
         const httpServer = http.createServer(app)
-        
+
         const server = new ApolloServer({
             resolvers: this.resolvers,
-            typeDefs
+            typeDefs,
+            context: ({ req, res }) => {
+                if (!req.headers.authorization) {
+                    return {
+                        currentuser: null,
+                        req,
+                        authorized: false
+                    }
+                }
+
+                const payload = jwt.verify(
+                    req.headers.authorization,
+                    process.env.JWT_KEY!,
+                )
+                return {
+                    currentUser: payload,
+                    req,
+                    authorized: !!payload
+                }
+            }
         });
 
         await server.start()
 
-        server.applyMiddleware({app})
+        server.applyMiddleware({ app })
 
-        return {httpServer, server}
+        return { httpServer, server }
     }
 }
 
