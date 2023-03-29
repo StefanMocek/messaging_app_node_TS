78b77efd246927028e47b6799cefd389bb989e64
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-03-29 15:58:18 +0000

    signin method added

--- src/auth/auth.resolvers.ts
+++ src/auth/auth.resolvers.ts
@@ -1,7 +1,8 @@
 import { Resolvers } from "../__generated__/resolvers-types";
 import {userService} from './user/user.service';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { GraphQLError } from "graphql";
+import bcrypt from 'bcrypt';
 
 export const authResolvers: Resolvers = {
     Mutation: {
@@ -14,6 +15,32 @@ export const authResolvers: Resolvers = {
                 )
             }
             const user = await userService.create(input);
+
+            const jwtToken = jwt.sign(
+                {email: input.email, userId: user.id}, 
+                process.env.JWT_KEY!,
+                {expiresIn: '7d'}
+            );
+
+            return {user, jwt: jwtToken};
+        },
+
+        async signin(parent, {input}, context) {
+            const user = await userService.findOneByEmail(input.email);
+            if(!user) {
+                throw new GraphQLError(
+                    'wrong credentials',
+                    {extensions: {code: 'BAD_REQUEST'}}    
+                )
+            }
+
+            const correctPwd = await bcrypt.compare(input.password, user.password)
+            if(!correctPwd) {
+                throw new GraphQLError(
+                    'wrong credentials',
+                    {extensions: {code: 'BAD_REQUEST'}}    
+                )
+            }
 
             const jwtToken = jwt.sign(
                 {email: input.email, userId: user.id}, 
