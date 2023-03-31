739d937762d05b656445c52d7a882446cb61ce66
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-03-31 19:50:10 +0000

    room.resolvers init

---
+++ src/rooms/room.resolvers.ts
@@ -0,0 +1,22 @@
+import { GraphQLError } from "graphql";
+import { Resolvers } from "../__generated__/resolvers-types";
+
+
+export const roomResolvers: Resolvers = {
+    Mutation: {
+        async sendMsg (parent, {input}, context) {
+            if(!context.authorized) {
+                throw new GraphQLError(
+                    'unauthorized',
+                    {extensions: {code: 'BAD_REQUEST'}}    
+                )
+            }
+            return {
+                id: input.roomId,
+                users: [],
+                messages: [{from: 1, content: input.message}]
+            }
+        }
+        }
+    }
+}
