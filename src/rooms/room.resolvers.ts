9e14a9b384dce202949fa0dd8c6b831cdef3e2bc
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-02 20:58:54 +0000

    sendMsg fixed and createRoom started

--- src/rooms/room.resolvers.ts
+++ src/rooms/room.resolvers.ts
@@ -1,21 +1,31 @@
 import { GraphQLError } from "graphql";
 import { Resolvers } from "../__generated__/resolvers-types";
+import { roomService } from "./room/room.service";
 
 
 export const roomResolvers: Resolvers = {
     Mutation: {
-        async sendMsg (parent, {input}, context) {
-            if(!context.authorized) {
+        async sendMsg(parent, { input }, context) {
+            if (!context.authorized) {
                 throw new GraphQLError(
                     'unauthorized',
-                    {extensions: {code: 'BAD_REQUEST'}}    
+                    { extensions: { code: 'BAD_REQUEST' } }
                 )
-            }
-            return {
-                id: input.roomId,
-                users: [],
-                messages: [{from: 1, content: input.message}]
-            }
+            };
+
+            return await roomService.addMessageToRoom(input.roomId, {
+                content: input.message,
+                from: context.currentUser.userId
+            })
+        },
+
+        async createRoom(parent, { input }, context) {
+            if (!context.authorized) {
+                throw new GraphQLError(
+                    'unauthorized',
+                    { extensions: { code: 'BAD_REQUEST' } }
+                )
+            };
         }
     }
 }
