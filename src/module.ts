a9998ba72090dae024c0f904bd295be671c0673e
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-04-03 18:10:46 +0000

    socket.io connection

--- src/module.ts
+++ src/module.ts
@@ -4,10 +4,12 @@ import express from "express";
 import jwt from 'jsonwebtoken';
 import { ApolloServer, ExpressContext } from "apollo-server-express";
 import { mergeResolvers } from '@graphql-tools/merge';
+import { Server } from 'socket.io';
 import { JwtPayload, Resolvers } from './__generated__/resolvers-types';
 import { authResolvers } from './auth/auth.resolvers';
 import { roomResolvers } from './rooms/room.resolvers';
 import { AppDataSource } from './app-data.source';
+import { Room } from './rooms/room/entity/room.entity';
 
 export interface MyContext extends ExpressContext {
     currentUser: JwtPayload;
@@ -18,18 +20,21 @@ export class AppModule {
     constructor(public resolvers: Resolvers) { }
 
     async startApollo(): Promise<{ httpServer: http.Server, server: ApolloServer<MyContext> }> {
-        await AppDataSource.initialize()
-
         const typeDefs = readFileSync('schema.graphql', { encoding: 'utf-8' });
-
+        
+        const appDataSoure = await AppDataSource.initialize()
         const app = express();
+        const httpServer = http.createServer(app);
 
-        const httpServer = http.createServer(app)
+        const io = new Server(httpServer);
+        io.on('connection', (socket) => {
+            app.request.socketIo = socket
+        })
 
         const server = new ApolloServer({
             resolvers: this.resolvers,
             typeDefs,
-            context: ({ req, res }) => {
+            context: async ({ req, res }) => {
                 let payload;
                 try {
                     payload = jwt.verify(
@@ -38,12 +43,24 @@ export class AppModule {
                     )
                 } catch (err) {
                     payload = null
+                };
+
+                if(typeof payload != 'string' && payload) {
+                    const rooms = await appDataSoure.manager.getRepository(Room)
+                        .createQueryBuilder('room')
+                        .innerJoin('room.users', 'u')
+                        .where('"u"."id" = :id', {id: payload.userId})
+                        .getMany();
+                    
+                    const roomsIds = rooms.map(room => `${room.id}`);
+                    req.socketIo?.join(roomsIds);
                 }
+
                 return {
                     currentUser: payload,
                     req,
                     authorized: !!payload
-                }
+                };
             }
         });
 
